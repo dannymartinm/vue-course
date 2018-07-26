f1bf669b8a428f9922e58a3ef36a7df600bd2029
Daniela Martin <danielamartinmtz@gmail.com>
2018-07-25 19:12:18 -0500

    First commit

---
+++ P2_vue/vue-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid";
+export default {
+  name: "app",
+  data: function() {
+    return {
+      quotes: ["Just a Quote"],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    appQuoteGrid: QuoteGrid
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ P2_vue/vue-cli/src/components/Quote.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+</script>
+<style>
+.panel-body {
+  font-family: "Courier New", Courier, monospace;
+  font-size: 24px;
+  color: #6e6e6e;
+}
+.quote {
+  cursor: pointer;
+}
+
+.panel {
+  border-style: "1px solid green";
+}
+.quote:hover {
+  background-color: #ffe2e2;
+}
+</style>
